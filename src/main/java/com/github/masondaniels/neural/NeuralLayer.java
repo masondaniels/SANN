dc82ba2ad68edefc1233348cad589879907935c4
Mason <masonmdaniels@gmail.com>
2020-05-17 21:36:18 -0400

    Whoops. This random shouldn't be seeded. I was debugging/making sure nothing broke.

--- src/main/java/com/github/masondaniels/neural/NeuralLayer.java
+++ src/main/java/com/github/masondaniels/neural/NeuralLayer.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class NeuralLayer {
 
-	private static Random random = new Random(1);
+	private static Random random = new Random();
 	private double bias = random.nextBoolean() ? random.nextDouble() : -(random.nextDouble());
 	private double[] neuronWeights;
 	private double[] neuronErrorSig;
